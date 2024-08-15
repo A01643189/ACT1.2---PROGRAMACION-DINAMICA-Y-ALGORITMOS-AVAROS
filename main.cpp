5f567cb64e336436990951e26045defdc9b7e7b3
A01643189 <a01643189@tec.mx>
2024-08-15 08:59:24 -0600

    Create MAIN and COMMENTS

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+// Francisco Javier Romo Juarez A01643189
+// Diego Enrique Vargas Ramírez A01635782
+
+// Cambio de monedas
+
+
+
+// Algoritmo Dinámico
+
+
+
+// Algoritmo Avaro
+
+// Main function
+
+int main() {
+    std::cout << "Numero de denominaciones" << std::endl;
+    int n;
+    std::cin >> n;
+    int denominaciones[n];
+    std::cout << "Denominaciones " << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> denominaciones[i];
+    }
+
+    return 0;
+}
